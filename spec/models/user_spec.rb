db587a52b6cdbb0e2177d8f028df112226b7e626
psanatov <psanatov@gmail.com>
2017-10-24 01:24:06 +0300

    Added User model and covered with unit tests

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,10 @@
+# spec/models/user_spec.rb
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  it { should have_many(:entries) }
+  
+	it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:email) }
+  it { should validate_presence_of(:password_digest) }
+end
