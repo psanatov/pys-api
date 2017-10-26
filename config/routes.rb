c7113201598fe3cb9a15b3d9b0e509cb7487a9aa
psanatov <psanatov@gmail.com>
2017-10-26 22:19:35 +0300

    Added versioning module & updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
+# config/routes
 Rails.application.routes.draw do
-  resources :entries do
-    resources :items
+
+  # namespace the controllers without affecting the URI
+  scope module: :v1, constraints: ApiVersion.new('v1', true) do
+    resources :entries do
+      resources :items
+    end
   end
 
   post 'auth/login', to: 'authentication#authenticate'
